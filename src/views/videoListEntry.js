37e6252142b3aba80fa5982655ccb96d16775e74
katm201 <katm201@gmail.com>
2017-10-23 19:50:36 -0700

    Got list views to appear. Not rendering correctly.

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,9 +1,13 @@
 var VideoListEntryView = Backbone.View.extend({
-
+  el: '.video-list',
+  
+  initialize() {
+    this.render();
+  },
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
-    return this;
+    return this.$el;
   },
 
   template: templateURL('src/templates/videoListEntry.html')
