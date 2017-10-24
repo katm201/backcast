ead1818f69760dc897b6b96c1323415ff8533a2c
katm201 <katm201@gmail.com>
2017-10-23 22:17:29 -0700

    Shows one video in videoList

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,21 +1,22 @@
 var VideoListView = Backbone.View.extend({
   
-  initialize: function() {
-    // put event handler for changing an individual video entry view
-    this.render();
-  },
+  // initialize: function() {
+  //   // put event handler for changing an individual video entry view
+
+  // },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    console.log(this.collection);
     this.collection.forEach(this.renderEntry, this);
     return this.$el;
   },
   
   renderEntry: function(video) {
-    let listEntry = new VideoListEntryView({model: video});
-    this.$el.append(listEntry);
-    return this.$el;
+    console.log(video);
+    new VideoListEntryView({ el: '.video-list', model: video }).render();
+    console.log(this.$el);
   },
 
   template: templateURL('src/templates/videoList.html')
