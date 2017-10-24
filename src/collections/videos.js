087c02f85f4e1649aa419c56502e3def0811617b
katm201 <katm201@gmail.com>
2017-10-23 23:29:23 -0700

    Show videos in video list

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -3,11 +3,3 @@ var Videos = Backbone.Collection.extend({
   model: Video
 
 });
-
-// var allVideos = window.exampleVideoData.map(function(video) {
-//   let newVideo = new Video(video);
-//   return newVideo;
-// });
-
-// var allVideosCollection = new Videos(allVideos);
-// console.log(allVideosCollection);
