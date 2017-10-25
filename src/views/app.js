aa10f5ae27c048b5b60cb4ffd5aadbc0980f04ce
katm201 <katm201@gmail.com>
2017-10-24 19:57:13 -0700

    Working on API integration.

--- src/views/app.js
+++ src/views/app.js
@@ -1,6 +1,8 @@
 var AppView = Backbone.View.extend({
 
   el: '#app',
+  
+  events: {'click': 'onSearch'},
 
   initialize: function() {
     this.videos = new Videos(window.exampleVideoData);
@@ -14,6 +16,32 @@ var AppView = Backbone.View.extend({
     new VideoPlayerView({ el: '.player', collection: this.videos }).render();
     new VideoListView({ el: '.list', collection: this.videos }).render();
     return this;
+  },
+  
+  fetchParams: {
+    key: window.YOUTUBE_API_KEY,
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    method: 'GET',
+    reset: true,
+    type: 'video',
+    data: {
+      part: 'snippet',
+      maxResults: '5',
+      q: 'puppies'
+    },
+    success: function(collection, data) {
+      this.videos = new Videos(data);
+      this.render();
+    },
+    failure: function(error) { 
+      console.log('Fetch failed ', error); 
+    }
+  },
+  
+  onSearch: function(query) {
+    console.log('app clicked');
+    var params = _.extend(this.fetchParams, { q: 'puppies' });
+    this.videos.fetch(this.fetchParams);
   },
 
   template: templateURL('src/templates/app.html')
