1d225578481b8d228ef201f165cf618b099ed97b
katm201 <katm201@gmail.com>
2017-10-24 17:18:55 -0700

    Player view re-rendering correctly with new selection!

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,13 +1,12 @@
 var VideoListEntryView = Backbone.View.extend({
 
   initialize: function() {
-    console.log(this.model);
     this.title = this.model.get('title');
     this.description = this.model.get('description');
     this.thumbnail = this.model.get('thumbnail');
   },
   
-  events: { 'select': 'handleSelect' }, 
+  events: { 'click': 'handleSelect' }, 
 
   render: function() {
     this.$el.html(this.template({title: this.title, description: this.description, thumbnail: this.thumbnail}));
