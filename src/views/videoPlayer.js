95a1366827849513636f36fb0b444532a54488f4
Hack Reactor Student <student@hackreactor.com>
2017-10-24 17:18:55 -0700

    Player view re-rendering correctly with new selection!

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -2,22 +2,25 @@ var VideoPlayerView = Backbone.View.extend({
 
   initialize: function() {
     this.newModel = this.collection.models[0];
+    this.setVideo(this.newModel);
     
-    this.title = this.newModel.get('title');
-    this.description = this.newModel.get('description');
-    this.vidID = this.newModel.get('id');
     this.baseSource = 'https://www.youtube.com/embed/';
     
+    this.collection.on('select', function(model) {
+      this.setVideo(model);
+      this.render();
+    }, this);
+  },
+  
+  setVideo: function(model) {
+    this.title = model.get('title');
+    this.description = model.get('description');
+    this.vidID = model.get('id');
   },
 
   render: function() {
     this.$el.html(this.template({source: (this.baseSource + this.vidID), title: this.title, description: this.description}));
     return this;
-  },
-  
-  handleSelection: function() {
-    debugger;
-    // re-render video player when select from a model fires off
   },
 
   template: templateURL('src/templates/videoPlayer.html')
